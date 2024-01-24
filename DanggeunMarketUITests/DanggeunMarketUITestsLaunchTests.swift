1d9ad5d831a6db5a3b2299947ade6705ef62d8d7
Wonder Park <pkkyung26@gmail.com>
2024-01-24 16:38:49 +0900

    Initial Commit

---
+++ DanggeunMarketUITests/DanggeunMarketUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DanggeunMarketUITestsLaunchTests.swift
+//  DanggeunMarketUITests
+//
+//  Created by 박희경 on 2024/01/24.
+//
+
+import XCTest
+
+final class DanggeunMarketUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
