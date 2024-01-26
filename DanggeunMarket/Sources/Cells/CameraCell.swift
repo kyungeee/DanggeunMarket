256727c1a27f4d9d5f0bf856f74752f397dd7c64
Wonder Park <pkkyung26@gmail.com>
2024-01-26 13:28:14 +0900

    [Add] 갤러리VC 에서 0번째 index 에 사용할 CameraCell 추가

---
+++ DanggeunMarket/Sources/Cells/CameraCell.swift
@@ -0,0 +1,60 @@
+//
+//  CameraCell.swift
+//  DanggeunMarket
+//
+//  Created by 박희경 on 2024/01/24.
+//
+
+import UIKit
+
+class CameraCell: UICollectionViewCell {
+    
+    //MARK: - View's LifeCycle
+    override func prepareForReuse() {
+        super.prepareForReuse()
+//        setUpAlbumCoverPlaceHolderImage()
+    }
+    
+    // MARK: - UI Components
+    static var cellIdentifier: String {
+        return String(describing: Self.self)
+    }
+    
+    lazy var cameraImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "camera")
+        return imageView
+    }()
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CameraCell {
+    private func setup(){
+        setCellAttribute()
+        setLayout()
+    }
+    
+    private func setCellAttribute() {
+        backgroundColor = .lightGray
+    }
+    
+    private func setLayout() {
+        self.contentView.addSubviews(cameraImage)
+        
+        NSLayoutConstraint.activate([
+            cameraImage.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            cameraImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
+ 
+}
